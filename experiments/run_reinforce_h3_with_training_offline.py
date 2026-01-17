75eadb5c5efd28a2e0427e7d06cf76504be9a849
sethkarten <seth.karten1@gmail.com>
2026-01-17 16:38:23 -0500

    Fix offline mode: allow HF Hub access for tokenizer metadata checks

--- experiments/run_reinforce_h3_with_training_offline.py
+++ experiments/run_reinforce_h3_with_training_offline.py
@@ -18,9 +18,11 @@ Usage:
 # Use vLLM legacy API to avoid V1 compilation issues
 import os
 os.environ['VLLM_USE_V1'] = '0'
-os.environ['HF_DATASETS_OFFLINE'] = '1'  # SLURM nodes don't have internet
-os.environ['HF_HUB_OFFLINE'] = '1'  # Prevent HuggingFace API calls
-os.environ['TRANSFORMERS_OFFLINE'] = '1'  # Force transformers to use cache only
+# CRITICAL: Allow online mode for tokenizer initialization
+# Models are cached locally, but transformers needs to check HF Hub for metadata
+os.environ['HF_DATASETS_OFFLINE'] = '0'
+os.environ['HF_HUB_OFFLINE'] = '0'
+os.environ['TRANSFORMERS_OFFLINE'] = '0'
 
 import argparse
 import asyncio
